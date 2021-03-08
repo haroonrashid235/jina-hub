e1d0d6b97c46d7c64c54bf990fcd5efa27ba3d4b
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-03-08 19:05:27 +0100

    feat: use single decorator for nlp crafters

--- crafters/nlp/TikaExtractor/tests/test_tikaextractor.py
+++ crafters/nlp/TikaExtractor/tests/test_tikaextractor.py
@@ -3,13 +3,17 @@ from .. import TikaExtractor
 
 def test_extraction_uri():
     tika_extractor = TikaExtractor()
-    extract = tika_extractor.craft(uri='cats_are_awesome.pdf', buffer=None)
-    assert len(extract['text']) > 20
+    crafted_docs = tika_extractor.craft(['cats_are_awesome.pdf', 'cats_are_awesome.pdf'], [None, None])
+    assert len(crafted_docs) == 2
+    for crafted_doc in crafted_docs:
+        assert len(crafted_doc['text']) > 20
 
 
 def test_extraction_bytes():
     tika_extractor = TikaExtractor()
     with open('cats_are_awesome.pdf', 'rb') as pdf:
         input_bytes = pdf.read()
-    extract = tika_extractor.craft(uri=None, buffer=input_bytes)
-    assert len(extract['text']) > 20
+    crafted_docs = tika_extractor.craft([None, None], [input_bytes, input_bytes])
+    assert len(crafted_docs) == 2
+    for crafted_doc in crafted_docs:
+        assert len(crafted_doc['text']) > 20
